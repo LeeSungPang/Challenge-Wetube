5b6ea562f0a0360e63146fc12718463a9ee3cdf9
LeeSungJun <qoik11@naver.com>
2020-06-25 17:10:07 +0900

    fake db and add controllers

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Challenge-Wetube
+
 I can Seccess challenge
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
+- [ ] Video Detail
+- [x] Edit Video
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import cookieParser from "cookie-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -9,14 +10,17 @@ import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
 
+app.use(localsMiddleware);
+
 app.use("/", globalRouter);
-// app.use("/users", userRouter);
+app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 export default app;
